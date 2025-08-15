26dcbc0a4b4c7472937112bae26201f37c9d678c
wierdthing <pkaratzhs2@gmail.com>
2025-08-15 19:36:00 +0300

    added dissolvent 2

--- Poisons.lua
+++ Poisons.lua
@@ -1,6 +1,8 @@
 function BUYPOISONS_LOADPOISONS()
-BuyPoisonsItemInfo={};	
-for i=1, 23, 1 do
+
+BuyPoisonsItemInfo={};
+
+for i=1, 24, 1 do
 	BuyPoisonsItemInfo[i]={};
 	BuyPoisonsItemInfo[i]["Components"]={};
 	BuyPoisonsItemInfo[i]["Components"][1]={};
@@ -8,19 +10,14 @@ for i=1, 23, 1 do
 end
 
 --Load language:
-if ( GetLocale() == "frFR" ) then
-	getglobal("BUYPOISONS_FR")();
-elseif ( GetLocale() == "deDE" ) then
-	getglobal("BUYPOISONS_DE")();
-else
-	getglobal("BUYPOISONS_EN")();
-end
+getglobal("BUYPOISONS_EN")();
+
 
 --Flash Powder
-BuyPoisonsItemInfo[22]["texture"] = "Interface\\Icons\\INV_Misc_Ammo_Gunpowder_03";
-BuyPoisonsItemInfo[22]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_FLASH_POWDER;
-BuyPoisonsItemInfo[22]["Components"][1]["Quantity"]= 1;
-BuyPoisonsItemInfo[22]["Vial_Type"]=nil;
+BuyPoisonsItemInfo[23]["texture"] = "Interface\\Icons\\INV_Misc_Ammo_Gunpowder_03";
+BuyPoisonsItemInfo[23]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_FLASH_POWDER;
+BuyPoisonsItemInfo[23]["Components"][1]["Quantity"]= 1;
+BuyPoisonsItemInfo[23]["Vial_Type"]=nil;
 
 --IP6
 BuyPoisonsItemInfo[2]["texture"] = "Interface\\Icons\\Ability_Poisons";
@@ -28,71 +25,71 @@ BuyPoisonsItemInfo[2]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DUST_OF_DET
 BuyPoisonsItemInfo[2]["Components"][1]["Quantity"]= 4;
 BuyPoisonsItemInfo[2]["Vial_Type"]=BUYPOISONS_VIAL_CRYSTAL;
 --IP5
-BuyPoisonsItemInfo[8]["texture"] = "Interface\\Icons\\Ability_Poisons";
-BuyPoisonsItemInfo[8]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DUST_OF_DETERIORATION;
-BuyPoisonsItemInfo[8]["Components"][1]["Quantity"]= 3;
-BuyPoisonsItemInfo[8]["Vial_Type"]=BUYPOISONS_VIAL_CRYSTAL;
+BuyPoisonsItemInfo[9]["texture"] = "Interface\\Icons\\Ability_Poisons";
+BuyPoisonsItemInfo[9]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DUST_OF_DETERIORATION;
+BuyPoisonsItemInfo[9]["Components"][1]["Quantity"]= 3;
+BuyPoisonsItemInfo[9]["Vial_Type"]=BUYPOISONS_VIAL_CRYSTAL;
 --IP4
-BuyPoisonsItemInfo[11]["texture"] = "Interface\\Icons\\Ability_Poisons";
-BuyPoisonsItemInfo[11]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DUST_OF_DETERIORATION;
-BuyPoisonsItemInfo[11]["Components"][1]["Quantity"]= 2;
-BuyPoisonsItemInfo[11]["Vial_Type"]=BUYPOISONS_VIAL_CRYSTAL;
+BuyPoisonsItemInfo[12]["texture"] = "Interface\\Icons\\Ability_Poisons";
+BuyPoisonsItemInfo[12]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DUST_OF_DETERIORATION;
+BuyPoisonsItemInfo[12]["Components"][1]["Quantity"]= 2;
+BuyPoisonsItemInfo[12]["Vial_Type"]=BUYPOISONS_VIAL_CRYSTAL;
 --IP3
-BuyPoisonsItemInfo[15]["texture"] = "Interface\\Icons\\Ability_Poisons";
-BuyPoisonsItemInfo[15]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DUST_OF_DETERIORATION;
-BuyPoisonsItemInfo[15]["Components"][1]["Quantity"]= 1;
-BuyPoisonsItemInfo[15]["Vial_Type"]=BUYPOISONS_VIAL_LEADED;
+BuyPoisonsItemInfo[16]["texture"] = "Interface\\Icons\\Ability_Poisons";
+BuyPoisonsItemInfo[16]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DUST_OF_DETERIORATION;
+BuyPoisonsItemInfo[16]["Components"][1]["Quantity"]= 1;
+BuyPoisonsItemInfo[16]["Vial_Type"]=BUYPOISONS_VIAL_LEADED;
 --IP2
-BuyPoisonsItemInfo[18]["texture"] = "Interface\\Icons\\Ability_Poisons";
-BuyPoisonsItemInfo[18]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DUST_OF_DECAY;
-BuyPoisonsItemInfo[18]["Components"][1]["Quantity"]= 3;
-BuyPoisonsItemInfo[18]["Vial_Type"]=BUYPOISONS_VIAL_LEADED;
+BuyPoisonsItemInfo[19]["texture"] = "Interface\\Icons\\Ability_Poisons";
+BuyPoisonsItemInfo[19]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DUST_OF_DECAY;
+BuyPoisonsItemInfo[19]["Components"][1]["Quantity"]= 3;
+BuyPoisonsItemInfo[19]["Vial_Type"]=BUYPOISONS_VIAL_LEADED;
 --IP1
-BuyPoisonsItemInfo[21]["texture"] = "Interface\\Icons\\Ability_Poisons";
-BuyPoisonsItemInfo[21]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DUST_OF_DECAY;
-BuyPoisonsItemInfo[21]["Components"][1]["Quantity"]= 1;
-BuyPoisonsItemInfo[21]["Vial_Type"]=BUYPOISONS_VIAL_EMPTY;
+BuyPoisonsItemInfo[22]["texture"] = "Interface\\Icons\\Ability_Poisons";
+BuyPoisonsItemInfo[22]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DUST_OF_DECAY;
+BuyPoisonsItemInfo[22]["Components"][1]["Quantity"]= 1;
+BuyPoisonsItemInfo[22]["Vial_Type"]=BUYPOISONS_VIAL_EMPTY;
 
 
 --CP2
-BuyPoisonsItemInfo[5]["texture"] = "Interface\\Icons\\Ability_PoisonSting";
-BuyPoisonsItemInfo[5]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_ESSENCE_OF_AGONY;
-BuyPoisonsItemInfo[5]["Components"][1]["Quantity"]= 3;
-BuyPoisonsItemInfo[5]["Vial_Type"]=BUYPOISONS_VIAL_CRYSTAL;
+BuyPoisonsItemInfo[6]["texture"] = "Interface\\Icons\\Ability_PoisonSting";
+BuyPoisonsItemInfo[6]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_ESSENCE_OF_AGONY;
+BuyPoisonsItemInfo[6]["Components"][1]["Quantity"]= 3;
+BuyPoisonsItemInfo[6]["Vial_Type"]=BUYPOISONS_VIAL_CRYSTAL;
 --CP1
-BuyPoisonsItemInfo[20]["texture"] = "Interface\\Icons\\Ability_PoisonSting";
-BuyPoisonsItemInfo[20]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_ESSENCE_OF_PAIN;
-BuyPoisonsItemInfo[20]["Components"][1]["Quantity"]= 1;
-BuyPoisonsItemInfo[20]["Vial_Type"]=BUYPOISONS_VIAL_EMPTY;
+BuyPoisonsItemInfo[21]["texture"] = "Interface\\Icons\\Ability_PoisonSting";
+BuyPoisonsItemInfo[21]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_ESSENCE_OF_PAIN;
+BuyPoisonsItemInfo[21]["Components"][1]["Quantity"]= 1;
+BuyPoisonsItemInfo[21]["Vial_Type"]=BUYPOISONS_VIAL_EMPTY;
 
 --WP4
-BuyPoisonsItemInfo[23]["texture"] = "Interface\\Icons\\INV_Misc_Herb_16";
-BuyPoisonsItemInfo[23]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_ESSENCE_OF_AGONY;
-BuyPoisonsItemInfo[23]["Components"][1]["Quantity"]= 2;
-BuyPoisonsItemInfo[23]["Components"][2]["Item"]= BUYPOISONS_COMPONENT_DEATHWEED;
-BuyPoisonsItemInfo[23]["Components"][2]["Quantity"]= 2;
-BuyPoisonsItemInfo[23]["Vial_Type"]=BUYPOISONS_VIAL_CRYSTAL;
+BuyPoisonsItemInfo[24]["texture"] = "Interface\\Icons\\INV_Misc_Herb_16";
+BuyPoisonsItemInfo[24]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_ESSENCE_OF_AGONY;
+BuyPoisonsItemInfo[24]["Components"][1]["Quantity"]= 2;
+BuyPoisonsItemInfo[24]["Components"][2]["Item"]= BUYPOISONS_COMPONENT_DEATHWEED;
+BuyPoisonsItemInfo[24]["Components"][2]["Quantity"]= 2;
+BuyPoisonsItemInfo[24]["Vial_Type"]=BUYPOISONS_VIAL_CRYSTAL;
 --WP3
-BuyPoisonsItemInfo[9]["texture"] = "Interface\\Icons\\INV_Misc_Herb_16";
-BuyPoisonsItemInfo[9]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_ESSENCE_OF_AGONY;
-BuyPoisonsItemInfo[9]["Components"][1]["Quantity"]= 1;
-BuyPoisonsItemInfo[9]["Components"][2]["Item"]= BUYPOISONS_COMPONENT_DEATHWEED;
-BuyPoisonsItemInfo[9]["Components"][2]["Quantity"]= 2;
-BuyPoisonsItemInfo[9]["Vial_Type"]=BUYPOISONS_VIAL_CRYSTAL;
+BuyPoisonsItemInfo[10]["texture"] = "Interface\\Icons\\INV_Misc_Herb_16";
+BuyPoisonsItemInfo[10]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_ESSENCE_OF_AGONY;
+BuyPoisonsItemInfo[10]["Components"][1]["Quantity"]= 1;
+BuyPoisonsItemInfo[10]["Components"][2]["Item"]= BUYPOISONS_COMPONENT_DEATHWEED;
+BuyPoisonsItemInfo[10]["Components"][2]["Quantity"]= 2;
+BuyPoisonsItemInfo[10]["Vial_Type"]=BUYPOISONS_VIAL_CRYSTAL;
 --WP2
-BuyPoisonsItemInfo[12]["texture"] = "Interface\\Icons\\INV_Misc_Herb_16";
-BuyPoisonsItemInfo[12]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_ESSENCE_OF_PAIN;
-BuyPoisonsItemInfo[12]["Components"][1]["Quantity"]= 1;
-BuyPoisonsItemInfo[12]["Components"][2]["Item"]= BUYPOISONS_COMPONENT_DEATHWEED;
-BuyPoisonsItemInfo[12]["Components"][2]["Quantity"]= 2;
-BuyPoisonsItemInfo[12]["Vial_Type"]=BUYPOISONS_VIAL_LEADED;
+BuyPoisonsItemInfo[13]["texture"] = "Interface\\Icons\\INV_Misc_Herb_16";
+BuyPoisonsItemInfo[13]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_ESSENCE_OF_PAIN;
+BuyPoisonsItemInfo[13]["Components"][1]["Quantity"]= 1;
+BuyPoisonsItemInfo[13]["Components"][2]["Item"]= BUYPOISONS_COMPONENT_DEATHWEED;
+BuyPoisonsItemInfo[13]["Components"][2]["Quantity"]= 2;
+BuyPoisonsItemInfo[13]["Vial_Type"]=BUYPOISONS_VIAL_LEADED;
 --WP
-BuyPoisonsItemInfo[16]["texture"] = "Interface\\Icons\\INV_Misc_Herb_16";
-BuyPoisonsItemInfo[16]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_ESSENCE_OF_PAIN;
-BuyPoisonsItemInfo[16]["Components"][1]["Quantity"]= 1;
-BuyPoisonsItemInfo[16]["Components"][2]["Item"]= BUYPOISONS_COMPONENT_DEATHWEED;
-BuyPoisonsItemInfo[16]["Components"][2]["Quantity"]= 1;
-BuyPoisonsItemInfo[16]["Vial_Type"]=BUYPOISONS_VIAL_LEADED;
+BuyPoisonsItemInfo[17]["texture"] = "Interface\\Icons\\INV_Misc_Herb_16";
+BuyPoisonsItemInfo[17]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_ESSENCE_OF_PAIN;
+BuyPoisonsItemInfo[17]["Components"][1]["Quantity"]= 1;
+BuyPoisonsItemInfo[17]["Components"][2]["Item"]= BUYPOISONS_COMPONENT_DEATHWEED;
+BuyPoisonsItemInfo[17]["Components"][2]["Quantity"]= 1;
+BuyPoisonsItemInfo[17]["Vial_Type"]=BUYPOISONS_VIAL_LEADED;
 
 
 --DP5 
@@ -101,53 +98,61 @@ BuyPoisonsItemInfo[1]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DEATHWEED;
 BuyPoisonsItemInfo[1]["Components"][1]["Quantity"]= 7;
 BuyPoisonsItemInfo[1]["Vial_Type"]=BUYPOISONS_VIAL_CRYSTAL;
 --DP4
-BuyPoisonsItemInfo[7]["texture"] = "Interface\\Icons\\Ability_Rogue_DualWeild";
-BuyPoisonsItemInfo[7]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DEATHWEED;
-BuyPoisonsItemInfo[7]["Components"][1]["Quantity"]= 5;
-BuyPoisonsItemInfo[7]["Vial_Type"]=BUYPOISONS_VIAL_CRYSTAL;
+BuyPoisonsItemInfo[8]["texture"] = "Interface\\Icons\\Ability_Rogue_DualWeild";
+BuyPoisonsItemInfo[8]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DEATHWEED;
+BuyPoisonsItemInfo[8]["Components"][1]["Quantity"]= 5;
+BuyPoisonsItemInfo[8]["Vial_Type"]=BUYPOISONS_VIAL_CRYSTAL;
 --DP3
-BuyPoisonsItemInfo[10]["texture"] = "Interface\\Icons\\Ability_Rogue_DualWeild";
-BuyPoisonsItemInfo[10]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DEATHWEED;
-BuyPoisonsItemInfo[10]["Components"][1]["Quantity"]= 3;
-BuyPoisonsItemInfo[10]["Vial_Type"]=BUYPOISONS_VIAL_CRYSTAL;
+BuyPoisonsItemInfo[11]["texture"] = "Interface\\Icons\\Ability_Rogue_DualWeild";
+BuyPoisonsItemInfo[11]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DEATHWEED;
+BuyPoisonsItemInfo[11]["Components"][1]["Quantity"]= 3;
+BuyPoisonsItemInfo[11]["Vial_Type"]=BUYPOISONS_VIAL_CRYSTAL;
 --DP2
-BuyPoisonsItemInfo[13]["texture"] = "Interface\\Icons\\Ability_Rogue_DualWeild";
-BuyPoisonsItemInfo[13]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DEATHWEED;
-BuyPoisonsItemInfo[13]["Components"][1]["Quantity"]= 2;
-BuyPoisonsItemInfo[13]["Vial_Type"]=BUYPOISONS_VIAL_LEADED;
---DP
-BuyPoisonsItemInfo[17]["texture"] = "Interface\\Icons\\Ability_Rogue_DualWeild";
-BuyPoisonsItemInfo[17]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DEATHWEED;
-BuyPoisonsItemInfo[17]["Components"][1]["Quantity"]= 1;
-BuyPoisonsItemInfo[17]["Vial_Type"]=BUYPOISONS_VIAL_LEADED;
-
---MP3
-BuyPoisonsItemInfo[4]["texture"] = "Interface\\Icons\\Spell_Nature_NullifyDisease";
-BuyPoisonsItemInfo[4]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DUST_OF_DETERIORATION;
-BuyPoisonsItemInfo[4]["Components"][1]["Quantity"]= 2;
-BuyPoisonsItemInfo[4]["Components"][2]["Item"]= BUYPOISONS_COMPONENT_ESSENCE_OF_AGONY;
-BuyPoisonsItemInfo[4]["Components"][2]["Quantity"]= 2;
-BuyPoisonsItemInfo[4]["Vial_Type"]=BUYPOISONS_VIAL_CRYSTAL;
---MP2
-BuyPoisonsItemInfo[14]["texture"] = "Interface\\Icons\\Spell_Nature_NullifyDisease";
-BuyPoisonsItemInfo[14]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DUST_OF_DECAY;
-BuyPoisonsItemInfo[14]["Components"][1]["Quantity"]=4;
-BuyPoisonsItemInfo[14]["Components"][2]["Item"]= BUYPOISONS_COMPONENT_ESSENCE_OF_PAIN;
-BuyPoisonsItemInfo[14]["Components"][2]["Quantity"]=4;
+BuyPoisonsItemInfo[14]["texture"] = "Interface\\Icons\\Ability_Rogue_DualWeild";
+BuyPoisonsItemInfo[14]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DEATHWEED;
+BuyPoisonsItemInfo[14]["Components"][1]["Quantity"]= 2;
 BuyPoisonsItemInfo[14]["Vial_Type"]=BUYPOISONS_VIAL_LEADED;
+--DP
+BuyPoisonsItemInfo[18]["texture"] = "Interface\\Icons\\Ability_Rogue_DualWeild";
+BuyPoisonsItemInfo[18]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DEATHWEED;
+BuyPoisonsItemInfo[18]["Components"][1]["Quantity"]= 1;
+BuyPoisonsItemInfo[18]["Vial_Type"]=BUYPOISONS_VIAL_LEADED;
+
+--DISSOL2 (New Dissolvent Poison II) - Position 4
+BuyPoisonsItemInfo[4]["texture"] = "Interface\\Icons\\Spell_Nature_CorrosiveBreath";
+BuyPoisonsItemInfo[4]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DUST_OF_DETERIORATION;
+BuyPoisonsItemInfo[4]["Components"][1]["Quantity"]= 3;
+BuyPoisonsItemInfo[4]["Components"][2]["Item"]= BUYPOISONS_COMPONENT_MAIDENS_ANGUISH;
+BuyPoisonsItemInfo[4]["Components"][2]["Quantity"]= 4;
+BuyPoisonsItemInfo[4]["Vial_Type"]=BUYPOISONS_VIAL_CRYSTAL;
+
+--MP3 
+BuyPoisonsItemInfo[5]["texture"] = "Interface\\Icons\\Spell_Nature_NullifyDisease";
+BuyPoisonsItemInfo[5]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DUST_OF_DETERIORATION;
+BuyPoisonsItemInfo[5]["Components"][1]["Quantity"]= 2;
+BuyPoisonsItemInfo[5]["Components"][2]["Item"]= BUYPOISONS_COMPONENT_ESSENCE_OF_AGONY;
+BuyPoisonsItemInfo[5]["Components"][2]["Quantity"]= 2;
+BuyPoisonsItemInfo[5]["Vial_Type"]=BUYPOISONS_VIAL_CRYSTAL;
+--MP2
+BuyPoisonsItemInfo[15]["texture"] = "Interface\\Icons\\Spell_Nature_NullifyDisease";
+BuyPoisonsItemInfo[15]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DUST_OF_DECAY;
+BuyPoisonsItemInfo[15]["Components"][1]["Quantity"]=4;
+BuyPoisonsItemInfo[15]["Components"][2]["Item"]= BUYPOISONS_COMPONENT_ESSENCE_OF_PAIN;
+BuyPoisonsItemInfo[15]["Components"][2]["Quantity"]=4;
+BuyPoisonsItemInfo[15]["Vial_Type"]=BUYPOISONS_VIAL_LEADED;
 --MP1
-BuyPoisonsItemInfo[19]["texture"] = "Interface\\Icons\\Spell_Nature_NullifyDisease";
-BuyPoisonsItemInfo[19]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DUST_OF_DECAY;
-BuyPoisonsItemInfo[19]["Components"][1]["Quantity"]=1;
-BuyPoisonsItemInfo[19]["Components"][2]["Item"]= BUYPOISONS_COMPONENT_ESSENCE_OF_PAIN;
-BuyPoisonsItemInfo[19]["Components"][2]["Quantity"]=1;
-BuyPoisonsItemInfo[19]["Vial_Type"]=BUYPOISONS_VIAL_EMPTY;
+BuyPoisonsItemInfo[20]["texture"] = "Interface\\Icons\\Spell_Nature_NullifyDisease";
+BuyPoisonsItemInfo[20]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_DUST_OF_DECAY;
+BuyPoisonsItemInfo[20]["Components"][1]["Quantity"]=1;
+BuyPoisonsItemInfo[20]["Components"][2]["Item"]= BUYPOISONS_COMPONENT_ESSENCE_OF_PAIN;
+BuyPoisonsItemInfo[20]["Components"][2]["Quantity"]=1;
+BuyPoisonsItemInfo[20]["Vial_Type"]=BUYPOISONS_VIAL_EMPTY;
 
 --AP1
-BuyPoisonsItemInfo[6]["texture"] = "Interface\\Icons\\Spell_Nature_NullifyPoison";
-BuyPoisonsItemInfo[6]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_MAIDENS_ANGUISH;
-BuyPoisonsItemInfo[6]["Components"][1]["Quantity"]= 2;
-BuyPoisonsItemInfo[6]["Vial_Type"]=BUYPOISONS_VIAL_LEADED;
+BuyPoisonsItemInfo[7]["texture"] = "Interface\\Icons\\Spell_Nature_NullifyPoison";
+BuyPoisonsItemInfo[7]["Components"][1]["Item"]= BUYPOISONS_COMPONENT_MAIDENS_ANGUISH;
+BuyPoisonsItemInfo[7]["Components"][1]["Quantity"]= 2;
+BuyPoisonsItemInfo[7]["Vial_Type"]=BUYPOISONS_VIAL_LEADED;
 
 --COR2
 BuyPoisonsItemInfo[3]["texture"] = "Interface\\Icons\\Spell_Nature_CorrosiveBreath";
@@ -156,6 +161,5 @@ BuyPoisonsItemInfo[3]["Components"][1]["Quantity"]= 3;
 BuyPoisonsItemInfo[3]["Components"][2]["Item"]= BUYPOISONS_COMPONENT_DEATHWEED;
 BuyPoisonsItemInfo[3]["Components"][2]["Quantity"]=3;
 BuyPoisonsItemInfo[3]["Vial_Type"]=BUYPOISONS_VIAL_CRYSTAL;
-
 
 end
--- localization_en.lua
+++ localization_en.lua
@@ -1,6 +1,5 @@
 function BUYPOISONS_EN()
 
-
 --Slash commands:
 SLASH_BUYPOISONS1							= "/buypoisons";
 SLASH_BUYPOISONS2							= "/bp";
@@ -16,6 +15,7 @@ BUYPOISONS_HELP_MESSAGE = {
 	"/bp ipX Y 	Buy Components for Y Level X Instant Poison",
 	"/bp ap1 Y  Buy Components for Y Agitating Poison",
 	"/bp fp Y Buy Y Flash Powder",
+	"/bp dissol2 Y Buy Components for Y Dissolvent Poison II",
 	"e.g. '/bp wp3 10' will buy all components needed for 10 lots of level 3 Wound Poison.",
 	"Warning: Using anything but multiples of 5 will not handle Vial Numbers properly",
 	"as they can only be bought in multiples of 5 from stores.",
@@ -47,11 +47,7 @@ BUYPOISONS_VIAL_EMPTY					="Empty Vial";
 BUYPOISONS_VIAL_CRYSTAL					="Crystal Vial";
 BUYPOISONS_VIAL_LEADED					="Leaded Vial";
 
-
-
 --Shortcut Key Commands
-
-
 BUYPOISONS_SHORTKEY_CRYSTAL_VIAL			="crystalvial";
 BUYPOISONS_SHORTKEY_DEATHWEED				="deathweed";
 BUYPOISONS_SHORTKEY_DUST_OF_DECAY			="dustofdecay";
@@ -64,52 +60,54 @@ BUYPOISONS_SHORTKEY_LEADED_VIAL				="leadedvial";
 BUYPOISONS_SHORTKEY_LETHARGY_ROOT			="lethargyroot";
 BUYPOISONS_SHORTKEY_MAIDENS_ANGUISH			="maidensanguish";
 
-
-
 BuyPoisonsItemInfo[1]["name"] 				= "Deadly Poison V";
 BuyPoisonsItemInfo[2]["name"] 				= "Instant Poison VI";
-BuyPoisonsItemInfo[3]["name"] 				= "Wound Poison IV";
-BuyPoisonsItemInfo[4]["name"] 				= "Mind-numbing Poison III";
-BuyPoisonsItemInfo[5]["name"] 				= "Crippling Poison II";
-BuyPoisonsItemInfo[6]["name"] 				= "Agitating Poison";
-BuyPoisonsItemInfo[7]["name"] 				= "Deadly Poison IV";
-BuyPoisonsItemInfo[8]["name"] 				= "Instant Poison V";
-BuyPoisonsItemInfo[9]["name"] 				= "Wound Poison III";
-BuyPoisonsItemInfo[10]["name"] 				= "Deadly Poison III";
-BuyPoisonsItemInfo[11]["name"] 				= "Instant Poison IV";
-BuyPoisonsItemInfo[12]["name"] 				= "Wound Poison II";
-BuyPoisonsItemInfo[13]["name"] 				= "Deadly Poison II";
-BuyPoisonsItemInfo[14]["name"] 				= "Mind-numbing Poison II";
-BuyPoisonsItemInfo[15]["name"] 				= "Instant Poison III";
-BuyPoisonsItemInfo[16]["name"] 				= "Wound Poison";
-BuyPoisonsItemInfo[17]["name"] 				= "Deadly Poison";
-BuyPoisonsItemInfo[18]["name"] 				= "Instant Poison II";
-BuyPoisonsItemInfo[19]["name"] 				= "Mind-numbing Poison";
-BuyPoisonsItemInfo[20]["name"] 				= "Crippling Poison";
-BuyPoisonsItemInfo[21]["name"] 				= "Instant Poison";
-BuyPoisonsItemInfo[22]["name"] 				= "Flash Powder";
+BuyPoisonsItemInfo[3]["name"] 				= "Corrosive Poison II";
+BuyPoisonsItemInfo[4]["name"] 				= "Dissolvent Poison II";
+BuyPoisonsItemInfo[5]["name"] 				= "Mind-numbing Poison III";
+BuyPoisonsItemInfo[6]["name"] 				= "Crippling Poison II";
+BuyPoisonsItemInfo[7]["name"] 				= "Agitating Poison";
+BuyPoisonsItemInfo[8]["name"] 				= "Deadly Poison IV";
+BuyPoisonsItemInfo[9]["name"] 				= "Instant Poison V";
+BuyPoisonsItemInfo[10]["name"] 				= "Wound Poison III";
+BuyPoisonsItemInfo[11]["name"] 				= "Deadly Poison III";
+BuyPoisonsItemInfo[12]["name"] 				= "Instant Poison IV";
+BuyPoisonsItemInfo[13]["name"] 				= "Wound Poison II";
+BuyPoisonsItemInfo[14]["name"] 				= "Deadly Poison II";
+BuyPoisonsItemInfo[15]["name"] 				= "Mind-numbing Poison II";
+BuyPoisonsItemInfo[16]["name"] 				= "Instant Poison III";
+BuyPoisonsItemInfo[17]["name"] 				= "Wound Poison";
+BuyPoisonsItemInfo[18]["name"] 				= "Deadly Poison";
+BuyPoisonsItemInfo[19]["name"] 				= "Instant Poison II";
+BuyPoisonsItemInfo[20]["name"] 				= "Mind-numbing Poison";
+BuyPoisonsItemInfo[21]["name"] 				= "Crippling Poison";
+BuyPoisonsItemInfo[22]["name"] 				= "Instant Poison";
+BuyPoisonsItemInfo[23]["name"] 				= "Flash Powder";
+BuyPoisonsItemInfo[24]["name"] 				= "Wound Poison IV";
 
 BuyPoisonsItemInfo[1]["shortkey"] = "dp5";
 BuyPoisonsItemInfo[2]["shortkey"] = "ip6";
-BuyPoisonsItemInfo[3]["shortkey"] = "wp4";
-BuyPoisonsItemInfo[4]["shortkey"] = "mp3";
-BuyPoisonsItemInfo[5]["shortkey"] = "cp2";
-BuyPoisonsItemInfo[6]["shortkey"] = "ap1";
-BuyPoisonsItemInfo[7]["shortkey"] = "dp4";
-BuyPoisonsItemInfo[8]["shortkey"] = "ip5";
-BuyPoisonsItemInfo[9]["shortkey"] = "wp3";
-BuyPoisonsItemInfo[10]["shortkey"] = "dp3";
-BuyPoisonsItemInfo[11]["shortkey"] = "ip4";
-BuyPoisonsItemInfo[12]["shortkey"] = "wp2";
-BuyPoisonsItemInfo[13]["shortkey"] = "dp2";
-BuyPoisonsItemInfo[14]["shortkey"] = "mp2";
-BuyPoisonsItemInfo[15]["shortkey"] = "ip3";
-BuyPoisonsItemInfo[16]["shortkey"] = "wp1";
-BuyPoisonsItemInfo[17]["shortkey"] = "dp1";
-BuyPoisonsItemInfo[18]["shortkey"] = "ip2";
-BuyPoisonsItemInfo[19]["shortkey"] = "mp1";
-BuyPoisonsItemInfo[20]["shortkey"] = "cp1";
-BuyPoisonsItemInfo[21]["shortkey"] = "ip1";
-BuyPoisonsItemInfo[22]["shortkey"] = "fp1";
+BuyPoisonsItemInfo[3]["shortkey"] = "cor2";
+BuyPoisonsItemInfo[4]["shortkey"] = "dissol2";
+BuyPoisonsItemInfo[5]["shortkey"] = "mp3";
+BuyPoisonsItemInfo[6]["shortkey"] = "cp2";
+BuyPoisonsItemInfo[7]["shortkey"] = "ap1";
+BuyPoisonsItemInfo[8]["shortkey"] = "dp4";
+BuyPoisonsItemInfo[9]["shortkey"] = "ip5";
+BuyPoisonsItemInfo[10]["shortkey"] = "wp3";
+BuyPoisonsItemInfo[11]["shortkey"] = "dp3";
+BuyPoisonsItemInfo[12]["shortkey"] = "ip4";
+BuyPoisonsItemInfo[13]["shortkey"] = "wp2";
+BuyPoisonsItemInfo[14]["shortkey"] = "dp2";
+BuyPoisonsItemInfo[15]["shortkey"] = "mp2";
+BuyPoisonsItemInfo[16]["shortkey"] = "ip3";
+BuyPoisonsItemInfo[17]["shortkey"] = "wp1";
+BuyPoisonsItemInfo[18]["shortkey"] = "dp1";
+BuyPoisonsItemInfo[19]["shortkey"] = "ip2";
+BuyPoisonsItemInfo[20]["shortkey"] = "mp1";
+BuyPoisonsItemInfo[21]["shortkey"] = "cp1";
+BuyPoisonsItemInfo[22]["shortkey"] = "ip1";
+BuyPoisonsItemInfo[23]["shortkey"] = "fp1";
+BuyPoisonsItemInfo[24]["shortkey"] = "wp4";
 
 end
